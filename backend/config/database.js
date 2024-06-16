e45b494420db28cfd100b16e4f4c984d7620760d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 18:35:47 +0530

    Error Handling (catch): If the connection attempt fails, the catch method's callback function is executed. This function logs the error to the console, providing information on what went wrong during the connection attempt.

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,6 +4,6 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log('Database connected');
     }).catch((error)=>{
-        
+        console.log(error);
     })
 };
