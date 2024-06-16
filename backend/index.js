c9979e385db899c90f25d010bf075e34ef0db6e9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 19:38:13 +0530

    Middleware Setup Body Parsing Middleware:
    app.use(express.urlencoded({ extended: true }));
    app.use(express.json());
    These middleware functions parse incoming request bodies.
    express.urlencoded({ extended: true }) parses incoming requests with URL-encoded payloads.
    express.json() parses incoming requests with JSON payloads. Together, they ensure that request data can be accessed in req.body.
    Cookie Parsing Middleware:
    
    app.use(cookieParser());
    Integrates the cookie-parser middleware into the application. This middleware parses cookies attached to the client request object and makes them available in req.cookies.

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ dotenv.config({});
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-
+// middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+app.use(cookieParser());
 
 
 
