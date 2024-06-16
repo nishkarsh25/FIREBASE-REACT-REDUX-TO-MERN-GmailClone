09e7b7e9a8fa3d4830037cb28e109eff6a94617d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 21:53:55 +0530

    Rendering Inbox Header:
    Toolbar: Renders a toolbar with several icons:
    MdCropSquare and FaCaretDown: Represents a dropdown or checkbox control.
    IoMdRefresh and IoMdMore: Represents refresh and more options buttons.
    Pagination: Displays pagination controls showing the current range of emails and navigation buttons (MdKeyboardArrowLeft and MdKeyboardArrowRight).

--- frontend/src/components/Inbox.jsx
+++ frontend/src/components/Inbox.jsx
@@ -26,6 +26,31 @@ const Inbox = () => {
     const [mailTypeSelected, setMailTypeSelected] = useState(0);
     const {emails} = useSelector(store=>store.app);
     
-    
+    return (
+        <div className='flex-1 bg-white rounded-xl mx-5'>
+            <div className='flex items-center justify-between px-4'>
+                <div className='flex items-center gap-2 text-gray-700 py-2'>
+                    <div className='flex items-center gap-1'>
+                        <MdCropSquare size={"20px"} />
+                        <FaCaretDown size={"20px"} />
+                    </div>
+                    <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+                        <IoMdRefresh size={"20px"} />
+                    </div>
+                    <div className='p-2 rounded-full hover:bg-gray-100 cursor-pointer'>
+                        <IoMdMore size={"20px"} />
+                    </div>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <p className='text-sm text-gray-500'>1-50 of {emails?.length}</p>
+                    <button disabled={false} className='hover:rounded-full hover:bg-gray-100'><MdKeyboardArrowLeft size={"24px"} /></button>
+                    <button disabled={false} className='hover:rounded-full hover:bg-gray-100'><MdKeyboardArrowRight size={"24px"} /></button>
+                </div>
+            </div>
+            
+                
+            </div>
+        </div>
+    )
 }
 
