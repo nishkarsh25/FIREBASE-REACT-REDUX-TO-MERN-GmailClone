1cb54fd6b77d8c5e21b1129a97174b7a37b584c0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 19:32:12 +0530

    Creating the Router Router Initialization: An Express router is created using express.Router(). A router is a modular, mini-application that can handle middleware and routes.

--- backend/routes/emailRoute.js
+++ backend/routes/emailRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createEmail, deleteEmail, getAllEmailById } from "../controllers/emailController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
-
+const router = express.Router();
 
 
 
