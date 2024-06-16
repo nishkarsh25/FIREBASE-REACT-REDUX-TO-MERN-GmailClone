e7e2b83988045957b8d1707c5385f3ebb1cdca85
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 21:29:18 +0530

    Imports React:
    React: Required for creating React components.
    useEffect: Hook for performing side effects in functional components.
    React Router:
    
    Outlet and useNavigate from react-router-dom:
    Outlet: Placeholder for nested route components.
    useNavigate: Hook for programmatically navigating between routes.
    Redux:
    
    useSelector from react-redux:
    Hook for accessing the Redux store and selecting parts of the state.

---
+++ frontend/src/components/Body.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Navbar from './Navbar'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+
+
+
