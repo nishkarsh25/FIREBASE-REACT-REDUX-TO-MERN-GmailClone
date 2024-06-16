96f013c0960a75d00987d1a205a814eee11d3605
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 21:56:36 +0530

    Imports React:
    React, useEffect, useState: Essential for creating React components and managing component lifecycle and state.
    Component Import:
    
    Message component: Presumably imported from './Message', responsible for rendering individual email messages.
    Redux:
    
    useSelector and useDispatch from react-redux: Hooks for accessing specific pieces of state from the Redux store and dispatching actions.
    Axios:
    
    axios: Library for making HTTP requests.

---
+++ frontend/src/components/Messages.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import Message from './Message';
+import { useDispatch, useSelector } from 'react-redux';
+import { setEmails } from '../redux/appSlice';
+import axios from 'axios';
+
+
+
+
