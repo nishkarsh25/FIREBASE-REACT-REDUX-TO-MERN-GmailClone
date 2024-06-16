cfc54003fdf4485974d6517710960c3d38f4ceb7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 22:04:17 +0530

    Rendering:
    motion.div:
    Utilizes Framer Motion's <motion.div> for animation effects:
    Initial: Starts with opacity 0 and moves up by 20 pixels (y: -20).
    Animate: Transitions to full opacity (opacity: 1) and normal position (y: 0).
    Transition: Animation duration of 0.5 seconds (duration: 0.5).
    onClick: Triggers the openMail function when clicked, allowing the user to view the selected email.

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -26,6 +26,14 @@ const Message = ({ email }) => {
     };
     const formattedDate = new Date(email?.createdAt).toLocaleString('en-IN', options);
 
-    
+    return (
+        <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            onClick={openMail} className="flex items-start justify-between border-b border-gray-200 px-4 py-3 text-sm hover:cursor-pointer hover:shadow-md">
+            
+        </motion.div>
+    )
 }
 
