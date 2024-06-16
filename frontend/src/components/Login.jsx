f3ad0467c199f0546a584b0f91641bd363684513
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 21:13:33 +0530

    Component Setup Imports:
    React hooks: useState for managing local component state.
    Link and useNavigate: From react-router-dom for navigation within the application.
    axios: For making HTTP requests to the server.
    toast: From react-hot-toast for displaying notifications.
    useDispatch: From react-redux to dispatch actions to Redux store.
    setAuthUser: Action creator imported from ../redux/appSlice to update the authenticated user in Redux state.

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import toast from "react-hot-toast";
+import { useDispatch } from 'react-redux';
+import { setAuthUser } from '../redux/appSlice';
+
+
+
+
