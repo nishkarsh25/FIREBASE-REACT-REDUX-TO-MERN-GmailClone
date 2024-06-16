12d7f31e05879af24c7349513ad4a74678489b71
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 19:38:30 +0530

    CORS Middleware:
    const corsOption={
        origin:'http://localhost:5173',
        credentials:true
    };
    
    app.use(cors(corsOption));
    Configures CORS (Cross-Origin Resource Sharing) for the Express application. It specifies that requests from http://localhost:5173 are allowed (origin), and sets credentials to true to allow credentials (like cookies) to be sent along with requests.

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,11 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
+const corsOption={
+    origin:'http://localhost:5173',
+    credentials:true
+};
+app.use(cors(corsOption)); 
 
 
 
