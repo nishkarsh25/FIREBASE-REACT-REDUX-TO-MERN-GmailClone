a753e1c69d79584ca98263e220ad0963ed1a29b3
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 20:01:44 +0530

    Imports React and ReactDOM:
    React: Required for JSX and React component creation.
    ReactDOM: Used for rendering React components into the DOM.
    App Component and Styling:
    
    App: Imported from './App.jsx', presumably the root component of the React application.
    './index.css': Imports styles for the application.
    Redux and Redux Persist:
    
    Provider: Component from react-redux that provides the Redux store to the React application.
    store: Imported from './redux/store.js', which is the configured Redux store.
    PersistGate: Component from redux-persist/integration/react that delays rendering of the application until persisted state has been retrieved and saved to Redux.
    persistStore: Function from redux-persist that creates a persistor object to manage state persistence.
    Toaster from react-hot-toast:
    
    Toaster: Component from react-hot-toast used for displaying toast notifications in the application.

---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+import { Toaster } from "react-hot-toast";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist';
+
+
+
