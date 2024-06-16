082d05f86b502dd8c59f255ece9cacdef047069b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 19:35:11 +0530

    Creating an Email createEmail Function: Purpose: Handles the creation of a new email document. Parameters: req (request object), res (response object). Logic: Extracting User ID: Retrieves the user ID from the request object (req.id). This ID is set by the isAuthenticated middleware and represents the authenticated user. Extracting Email Data: Destructures the to, subject, and message fields from the request body. Validation: Checks if any of the required fields (to, subject, message) are missing. If any field is missing, it responds with a 400 status code and an error message. Creating Email: Uses the Email.create method to create a new email document in the database with the provided to, subject, message, and userId. Response: Returns a 201 status code and the created email document in the response. Error Handling: Catches any errors and logs them to the console.

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -1,6 +1,29 @@
 import { Email } from "../models/emailModel.js";
 
+export const createEmail = async (req, res) => {
+  try {
+    const userId = req.id;
+    console.log(1)
+    const { to, subject, message } = req.body;
 
+    if (!to || !subject || !message) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const email = await Email.create({
+      to,
+      subject,
+      message,
+      userId,
+    });
+
+    return res.status(201).json({
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 
 
