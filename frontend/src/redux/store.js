df249ed618df0e49ffa1e8339db97bdafefc4bf7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 19:59:29 +0530

    Persistence Configuration persistConfig: Defines the configuration object for redux-persist. key: Specifies the key under which the persisted state will be stored in storage. version: Indicates the version of the persisted state. Incrementing this number will trigger migrations if needed. storage: Specifies where the persisted state will be stored (localStorage in this case).

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -11,6 +11,15 @@ import {
   } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+
+  const rootReducer = combineReducers({
+    app:appSlice
+ })
 
 
 
