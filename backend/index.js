c45c60baa62de25d7b8aa60ee454feb1e5ff95db
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 19:36:31 +0530

    Importing Dependencies Express:
    import express from "express";
    Express is imported as a module. It's a web application framework for Node.js that provides a robust set of features for building web applications and APIs.
    dotenv:
    
    import dotenv from "dotenv";
    dotenv is imported to handle environment variables from a .env file, allowing configuration settings to be loaded into process.env.
    connectDB:
    
    import connectDB from "./config/database.js";
    connectDB is imported, which likely includes code to establish a connection to a MongoDB database using Mongoose.
    Routes:
    
    import userRoute from "./routes/userRoute.js";
    import emailRoute from "./routes/emailRoute.js";
    These import statements bring in route handlers defined in separate files (userRoute.js and emailRoute.js). These files likely define the endpoints and route handlers for user-related and email-related functionalities.
    Middleware:
    
    import cookieParser from "cookie-parser";
    import cors from "cors";
    Middleware modules are imported to enhance the functionality of the Express application.
    cookie-parser: Parses cookies attached to the client request object.
    cors: Enables Cross-Origin Resource Sharing, allowing the application to accept requests from a different origin (e.g., frontend applications running on different ports or domains).

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+// const express = require('express')// method-1
+import express from "express"; // method-2
+import dotenv from "dotenv"; 
+import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import emailRoute from "./routes/emailRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+
+ 
+
+
+
+
+
+
+
+
