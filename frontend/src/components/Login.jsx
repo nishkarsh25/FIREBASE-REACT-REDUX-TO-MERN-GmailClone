462d679b2cac2dee4d194b2c1ad70668d480f086
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 21:15:43 +0530

    onSubmitHandler Function onSubmitHandler Function:
    async (e): Asynchronous function triggered on form submission.
    e.preventDefault(): Prevents the default form submission behavior to handle it manually.
    HTTP Request (axios.post):
    Sends a POST request to 'http://localhost:8080/api/v1/user/login' with input data (email and password).
    Specifies 'Content-Type': 'application/json' in headers.
    Includes withCredentials: true to enable sending cookies with the request.
    Handling Response:
    If successful (res.data exists):
    Dispatches setAuthUser(res.data.user) to update the Redux store with the authenticated user data.
    Navigates to the home route (navigate("/")).
    Displays a success toast notification (toast.success(res.data.message)).
    If error occurs:
    Displays an error toast notification with the error message from the server (toast.error(error.response.data.message)).
    Logs the error to the console for debugging.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,8 +13,30 @@ const Login = () => {
   const dispatch =useDispatch()
   const navigate = useNavigate();
 
-  
-  
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:8080/api/v1/user/login', input, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      });
+      if(res.data)
+        {
+            dispatch(setAuthUser(res.data.user))
+            navigate("/")
+            toast.success(res.data.message);
+        }
+    } catch (error) {
+        toast.error(error.response.data.message);
+      console.error(error.response.data.message);
+    }
+    setInput({
+      email: '',
+      password: '',
+    });
+  };
 
   
 };
