149243e651eafc01ff3dfc1a5f793dc89fd22f9d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-16 22:03:12 +0530

    Imports React:
    React: Required for creating React components.
    Icons:
    
    MdCropSquare from "react-icons/md": Represents a square icon.
    RiStarLine from "react-icons/ri": Represents a star icon.
    Redux and Routing:
    
    useDispatch from "react-redux": Hook used for dispatching actions to the Redux store.
    useNavigate from "react-router-dom": Hook used for imperative navigation.
    Animation:
    
    motion from "framer-motion": Library for creating animations in React components.

---
+++ frontend/src/components/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { MdCropSquare } from "react-icons/md";
+import { RiStarLine } from "react-icons/ri";
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setSelectedMail } from '../redux/appSlice';
+import { motion } from 'framer-motion';
+
+
